0cef2a271b05df132315e0764659410c66b64196
trandinhxuanthanh <trandinhxuanthanh@gmail.com>
2024-03-13 15:13:19 +0700

    Bucket name & cluster name updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,12 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  # default     = "us-east-2"
+  default     = "ap-southeast-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "kitops-eks"
+  default     = "vprofile-eks"
 }
