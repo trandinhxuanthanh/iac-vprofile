ce493dd143393ca7d8a7082a796cced6ce2069d8
trandinhxuanthanh <trandinhxuanthanh@gmail.com>
2024-03-14 14:33:18 +0700

    terraform fmt before commit is done

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "region" {
   description = "AWS region"
   type        = string
   # default     = "us-east-2"
-  default     = "ap-southeast-1"
+  default = "ap-southeast-1"
 }
 
 variable "clusterName" {
